170c12eb8d07bdde04bb669543954dfe01564357
annkimani <ann.wangari@strathmore.edu>
2024-09-26 21:54:35 +0300

    classification of users and saving it to a new excel spreadsheet.

---
+++ automation/customer_segmentation.py
@@ -0,0 +1,20 @@
+#this file contains logic for classifying customers into high,medium and low segments
+#usinf customer_csv file.
+from sympy import false
+
+from automation.loyalty_program import df
+
+
+def segment_customers(row):
+    if row['annual_income'] > 60000 and row['loyalty_score'] > 6.5 and row['purchase_frequency'] > 23.5:
+        return 'High customer'
+    elif row['annual_income'] > 45000 and row['loyalty_score'] > 4.5 and row['purchase_frequency'] > 14.5:
+        return 'Medium customer'
+    else:
+        return 'Low customer'
+
+#Apply segmentation to logic
+df['segment'] = df.apply(segment_customers, axis=1)
+
+#Saving the updated DataFrame to a new Excel spreadsheet
+df.to_excel('customer_segregation,xlsx', index = False)
