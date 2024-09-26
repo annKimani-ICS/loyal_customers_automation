7b2a969f76170477bddf9fc761ea226021ea2baa
annkimani <ann.wangari@strathmore.edu>
2024-09-26 21:26:47 +0300

    Automating reading data from dataset

--- automation/loyalty_program.py
+++ automation/loyalty_program.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+#Reading the dataset
+df = pd.read_csv(r'C:\Users\Ann Wangari\Desktop\Safcom_Projects\automation\customer_data.csv')
+
+#segment customers based on loyalty score
+
