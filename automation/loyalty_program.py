4ec9b642c5c9ac4b9298484412836614ae408942
annkimani <ann.wangari@strathmore.edu>
2024-09-30 18:59:23 +0300

    reading the dataset and customer segmentation

--- automation/loyalty_program.py
+++ automation/loyalty_program.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from customer_segmentation import segment_customers
 
+
 #Reading the dataset
 df = pd.read_csv(r'C:\Users\Ann Wangari\Desktop\Safcom_Projects\automation\customer_data.csv')
 
 #segment customers based on loyalty score
+segmented_df = segment_customers(df)
 
+#save segmented data to a new file
+segmented_df.to_excel('data/segmented_customers.xlsx', index=False)
